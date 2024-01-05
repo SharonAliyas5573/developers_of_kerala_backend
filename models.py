88439b5dcf5c45f222e3951b655d2f8e396c71cb
SharonAliyas5573 <sharonaliyas25@gmail.com>
2024-01-05 13:55:25 +0530

    feat: Enhance Company-Opening Relationship

--- models.py
+++ models.py
@@ -12,17 +12,14 @@ class UserRegistration(BaseModel):
     username: str
     email: EmailStr
     password: str
-    role: UserRole = UserRole.developer # default role is developer
-
-class UserProfileUpdate(BaseModel):
+    role: UserRole = UserRole.developer 
     full_name: str
     profile_pic: str  # s3 address for profile picture
-    contact: str
-    #add more fields here
+    contact: str# default role is developer
 
 
 # Developer-specific fields
-class DeveloperProfileUpdate(UserProfileUpdate):
+class DeveloperProfileUpdate(UserRegistration):
     intro: str
     skills: List[str]
     experience: int
@@ -33,21 +30,25 @@ class DeveloperProfileUpdate(UserProfileUpdate):
     achievements: str
     projects: List[str]
     socials: dict  # e.g., {"LinkedIn": "<link>", "GitHub": "<link>", "Twitter": "<link>"}
-    
-    
-# Company-specific fields
-class CompanyProfileUpdate(UserProfileUpdate):
-    industry: str
-    detail_intro: str
-    location: str
-    open_positions: dict  # e.g., {"Software Engineer": 3, "Data Scientist": 2}
+ 
+class Opening(BaseModel):
     skills_needed: List[str]
     qualification_required: str
     job_role: str
-    job_description: str
+    job_description: str   
     
+# Company-specific fields
+class CompanyProfileUpdate(UserRegistration):
+    industry: str
+    detail_intro: str
+    location: str
+    openings: List[Opening] = []
+    socials: dict  # e.g., {"LinkedIn": "<link>", "GitHub": "<link>", "Twitter": "<link>"}
+    
+
 class UserReference(BaseModel):
     user_info: UserRegistration
-    profile_info: UserProfileUpdate
     developer_info: DeveloperProfileUpdate = None
     company_info: CompanyProfileUpdate = None
+
+
